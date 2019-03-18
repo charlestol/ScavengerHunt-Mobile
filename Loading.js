abf629e1b89b173dd6c4667ef2f4a158c9538694
myprecioooous <pdelosreyes@ucmerced.edu>
2019-03-18 09:51:12 -0700

    Login/sign up first attempt

---
+++ Loading.js
@@ -0,0 +1,30 @@
+// Loading.js
+//Screen that displays until we determine the auth state of a user
+import React from 'react'
+import { View, Text, ActivityIndicator, StyleSheet } from 'react-native'
+import firebase from 'firebase';
+require('./src/config')
+
+export default class Loading extends React.Component {
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      this.props.navigation.navigate(user ? 'Main' : 'SignUp')
+    })
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Loading</Text>
+        <ActivityIndicator size="large" />
+      </View>
+    )
+  }
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+})
---
+++ src/config.js
@@ -0,0 +1,13 @@
+import firebase from 'firebase';
+// require("firebase/firestore");
+
+let config = {
+    apiKey: "AIzaSyB_BOnrLIbOsgFf2U84pOXYJagwGPltLaM",
+    authDomain: "cziscavengerhunt.firebaseapp.com",
+    databaseURL: "https://cziscavengerhunt.firebaseio.com",
+    projectId: "cziscavengerhunt",
+    storageBucket: "cziscavengerhunt.appspot.com",
+    messagingSenderId: "812384034022"
+};
+
+firebase.initializeApp(config);
