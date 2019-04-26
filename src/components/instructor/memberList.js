dea55657b1bab0ee637ab7f730dc51b0bf2f911c
myprecioooous <pdelosreyes@ucmerced.edu>
2019-04-26 13:03:50 -0700

    display member works

--- src/components/instructor/memberList.js
+++ src/components/instructor/memberList.js
@@ -1,13 +1,43 @@
 import React, { Component } from 'react';  
 import { View, Text, StyleSheet, Button } from 'react-native';
+import firebase from 'firebase/app';
+import 'firebase/firestore'
+
+require('../../config')
+const db = firebase.firestore();
 
 export default class ListMember extends Component {  
+    state = {members: []}
+
+    componentDidMount() {
+        let ac = this.props.navigation.state.params.accessCode
+        // console.log(this.props.navigation.state.params.accessCode)
+        db.collection("scavengerHunts").doc(ac).collection('members')
+        .onSnapshot(querySnapshot => {
+            let members = [];
+            querySnapshot.forEach(doc => {
+                let data = doc.data();
+                members.push(data);
+            });
+            // console.log(querySnapshot)
+            this.setState({
+              members,
+            });
+          });
+    }
+
   render() {
     //   console.log('list')
-    let ac = this.props.navigation.state.params.accessCode
+    // let ac = this.props.navigation.state.params.accessCode
+    const { members } = this.state;
     return (
       <View style={styles.container}>
         <Text>Member List</Text>
+        {members.map(member => (
+            <Text key={member.email}>
+                {member.name}
+            </Text>
+        ))}
         <Button
             title='Back'
             onPress={() => {
