97979ea277dd70a1f9f305e0dc5b1304dc4537bf
BKhush <khushpreetbuttar@gmail.com>
2019-04-27 18:05:05 -0700

    login UI

--- src/components/auth/loading.js
+++ src/components/auth/loading.js
@@ -1,6 +1,7 @@
 // Loading.js
 //Screen that displays until we determine the auth state of a user
 import React from 'react'
+import { Font } from 'expo';
 import { View, Text, ActivityIndicator, StyleSheet } from 'react-native'
 require('../../config')
 import firebase from 'firebase'
@@ -8,7 +9,22 @@ import 'firebase/firestore'
 const db = firebase.firestore()
 
 export default class Loading extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      isReady: false
+    };
+  }
   componentDidMount() {
+    Font.loadAsync({
+      Roboto: require("native-base/Fonts/Roboto.ttf"),
+      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+      Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf"),
+      Entypo: require("native-base/Fonts/Entypo.ttf"),
+      Feather: require("native-base/Fonts/Feather.ttf"),
+      FontAwesome: require("native-base/Fonts/FontAwesome.ttf"),
+    });
+    this.setState({ isReady: true });
     firebase.auth().onAuthStateChanged(user => {
       if(!user) {
         this.props.navigation.navigate('Login')
