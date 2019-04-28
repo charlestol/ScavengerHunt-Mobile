c4694b8792b7dd9563bb243d9165379e407d4110
BKhush <khushpreetbuttar@gmail.com>
2019-04-27 19:54:01 -0700

    sign up UI

--- src/components/auth/signUp.js
+++ src/components/auth/signUp.js
@@ -1,6 +1,22 @@
 // SignUp.js
 import React from 'react'
-import { StyleSheet, Text, TextInput, View, Button } from 'react-native'
+import { StyleSheet, View} from 'react-native'
+import {
+  Container,
+  Content,
+  Button,
+  Item,
+  Label,
+  Input,
+  Form,
+  Text, 
+  H1,
+  Icon,
+  Radio,
+  ListItem,
+  Left,
+  Right
+} from "native-base";
 import firebase from 'firebase';
 import 'firebase/firestore'
 require('../../config')
@@ -27,24 +43,24 @@ handleSignUp = () => {
         //   displayName: firstName,
         // })
         db.collection("users").doc(user.email).set(
-         (userType==='student') ? {
-           firstName: firstName,
-           lastName: lastName,
-           email: email,
-           studentID: studentID,
-           uid: user.uid,
-           userType: userType
-         } : {
-           firstName: firstName,
-           lastName: lastName,
-           email: email,
-           uid: user.uid,
-           userType: userType
-         }
-       )
-       .catch(function(error) {
-         console.error("Error writing document: ", error);
-       })
+          (userType==='student') ? {
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+            studentID: studentID,
+            uid: user.uid,
+            userType: userType
+          } : {
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+            uid: user.uid,
+            userType: userType
+          }
+        )
+        .catch(function(error) {
+          console.error("Error writing document: ", error);
+        })
       })
       .then(() => {
         if(this.state.userType === 'student') {
@@ -74,76 +90,116 @@ onClickInstructor = () => {
 
 render() {
     return (
-      <View style={styles.container}>
-        <Text>Sign Up</Text>
+      <Container >
+      <Content>
+        <H1 style={{marginTop: 100, textAlign: "center"}}>Sign up</H1>
+        
+        <View style={{
+            flexDirection: "row",
+            marginRight: 50,
+            marginLeft: 50,
+            marginTop: 10,
+            }}>
+            <Button 
+            style={{marginLeft: 55, marginRight: 10}}
+            bordered
+            onPress={this.onClickStudent}
+            >
+              <Text>Student</Text>
+            </Button>
+            <Button 
+            onPress={this.onClickInstructor}
+            bordered
+            >
+              <Text>Instructor</Text>
+            </Button>
+          </View>
+
+        <Form style={{marginLeft: 35, marginRight: 35}}>
         {this.state.errorMessage &&
           <Text style={{ color: 'red' }}>
             {this.state.errorMessage}
           </Text>}
-        <View style={{flexDirection:'row'}}>
-          <Button title="Student" onPress={this.onClickStudent}/>
-          <Button title="Instructor" onPress={this.onClickInstructor}/>
-        </View>
+        <Item floatingLabel>
+            <Label>First Name</Label>
+            <Input 
+            autoCapitalize="none"
+            style={styles.textInput}
+            onChangeText={firstName => this.setState({ firstName })}
+            value={this.state.firstName}
+            />
+          </Item>
+          <Item floatingLabel>
+            <Label>Last Name</Label>
+            <Input
+            autoCapitalize="none"
+            style={styles.textInput}
+            onChangeText={lastName => this.setState({ lastName })}
+            value={this.state.lastName}
+            />
+          </Item>
 
-        <TextInput
-          placeholder="First Name"
-          autoCapitalize="none"
-          style={styles.textInput}
-          onChangeText={firstName => this.setState({ firstName })}
-          value={this.state.firstName}
-        />
-        <TextInput
-          placeholder="Last Name"
-          autoCapitalize="none"
-          style={styles.textInput}
-          onChangeText={lastName => this.setState({ lastName })}
-          value={this.state.lastName}
-        />
         {this.state.userType === 'student' &&
-          <TextInput
-            placeholder="Student ID"
+          <Item floatingLabel>
+            <Label>Student ID</Label>
+            <Input
             autoCapitalize="none"
             style={styles.textInput}
             onChangeText={studentID => this.setState({ studentID })}
             value={this.state.studentID}
-          />
-        }
-        <TextInput
-          placeholder="Email"
-          autoCapitalize="none"
-          style={styles.textInput}
-          onChangeText={email => this.setState({ email })}
-          value={this.state.email}
-        />
-        <TextInput
-          secureTextEntry
-          placeholder="Password"
-          autoCapitalize="none"
-          style={styles.textInput}
-          onChangeText={password => this.setState({ password })}
-          value={this.state.password}
-        />
-        <Button title="Sign Up" onPress={this.handleSignUp} />
-        <Button
-          title="Already have an account? Login"
-          onPress={() => this.props.navigation.navigate('Login')}
-        />
-      </View>
+            />
+          </Item>
+          }
 
+          <Item floatingLabel>
+            <Label>Email</Label>
+            <Input
+            autoCapitalize="none"
+            style={styles.textInput}
+            onChangeText={email => this.setState({ email })}
+            value={this.state.email}
+            />
+          </Item>
+          <Item floatingLabel last>
+            <Label>Password</Label>
+            <Input
+            secureTextEntry
+            autoCapitalize="none"
+            style={styles.textInput}
+            onChangeText={password => this.setState({ password })}
+            value={this.state.password}
+            />
+          </Item>
+        </Form>
+        
+        <Button
+            block
+            onPress={this.handleSignUp} 
+            style={styles.marginBtn}
+          >
+            <Text>Sign Up</Text>
+        </Button>
+        <Button
+            block
+            transparent
+            onPress={() => this.props.navigation.navigate('Login')}
+            style={styles.marginBtn}
+          >
+            <Text>Already have an account? Sign in</Text>
+        </Button>
+      </Content>
+    </Container>
     )
   }
 }
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+    backgroundColor: "#FFF",
   },
-  textInput: {
-    height: 40,
-    width: '90%',
-    borderColor: 'gray',
-    borderWidth: 1,
-    marginTop: 8
+  marginBtn: {
+      marginTop: 50,
+      marginRight: 50,
+      marginLeft: 50
   }
+
 })
