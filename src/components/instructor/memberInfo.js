7a01a51502cb88d06141938c226f19f488ba9eba
myprecioooous <pdelosreyes@ucmerced.edu>
2019-04-28 18:42:30 -0700

    recentered member info

--- src/components/instructor/memberInfo.js
+++ src/components/instructor/memberInfo.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';  
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, Button } from 'react-native';
 
 export default class MemberInfo extends Component {  
   render() {
     return (
-      <View>
+      <View style={styles.container}>
         <Text>Member Info</Text>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
