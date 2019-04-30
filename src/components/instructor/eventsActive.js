78b2b2b3bf56c734d0cc2f9b5ad33e43af3392f5
myprecioooous <pdelosreyes@ucmerced.edu>
2019-04-29 20:32:11 -0700

    active events created

---
+++ src/components/instructor/eventsActive.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';  
+import { View, Text } from 'react-native';
+
+export default class ActiveEvents extends Component {  
+  render() {
+    return (
+      <View>
+        <Text>Active Events</Text>
+      </View>
+    );
+  }
+}
