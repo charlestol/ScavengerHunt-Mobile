c5f26fee49570e35d8ec5b11113c9b36579e352d
myprecioooous <pdelosreyes@ucmerced.edu>
2019-04-29 20:54:57 -0700

    active events working

--- src/components/instructor/eventsActive.js
+++ src/components/instructor/eventsActive.js
@@ -1,12 +1,66 @@
 import React, { Component } from 'react';  
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
+import firebase from 'firebase/app';
+import { withNavigation } from 'react-navigation';
 
-export default class ActiveEvents extends Component {  
+import 'firebase/firestore'
+require('../../config')
+const db = firebase.firestore();
+
+class ActiveEvents extends Component {  
+  state = { activeEvents: [] };
+
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(user => {
+    if(user === null) {
+      return;
+    }  
+    
+    const today = new Date();
+
+    db.collection('scavengerHunts').where("email", "==", user.email)
+    .onSnapshot(snapshot => {
+      let activeEvents = [];
+
+      snapshot.forEach(doc => {
+        let data = doc.data();
+          const closed = data.closed;
+          const endDate = data.dateEnd.seconds;
+          // convert millisec to sec
+          const today = (Date.now() / 1000).toFixed(0);
+
+          if(today < endDate && !closed) {
+            activeEvents.push(data);
+          }
+      });
+
+      this.setState({
+        activeEvents
+      });
+    });})
+  }
+
   render() {
+    const { activeEvents } = this.state;
+
     return (
       <View>
         <Text>Active Events</Text>
+        {activeEvents.map(scavengerHunt => (
+            <View key={scavengerHunt.accessCode}>
+              <Button
+                title={scavengerHunt.name}
+                onPress={() => {
+                  this.props.navigation.navigate('IEventItem', {
+                    accessCode: scavengerHunt.accessCode,
+                  })
+                }}
+              />
+            </View>
+          ))}
       </View>
     );
   }
 }
+
+export default withNavigation(ActiveEvents)
